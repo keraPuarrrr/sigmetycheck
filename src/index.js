113e8a76240e67e026605eca6343af0db611958c
keraPuarrrr <159476321+keraPuarrrr@users.noreply.github.com>
2024-03-05 21:18:51 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// sigmetycheck.js
+
+// Require the sigmety logger package
+const logger = require('sigmety');
+
+// Log some messages using the logger
+logger.info('This is an informational message from sigmetycheck.');
+logger.warn('This is a warning message from sigmetycheck.');
+logger.error('This is an error message from sigmetycheck.');
